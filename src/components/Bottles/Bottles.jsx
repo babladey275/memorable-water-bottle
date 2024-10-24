32649ff141aad9ac8bbf723bd39f6168a35b6cf9
babladey275 <babladey275@gmail.com>
2024-10-24 11:40:54 +0600

    added event handler

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -5,6 +5,7 @@ import './Bottles.css'
 
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('bottles.json')
@@ -12,13 +13,20 @@ const Bottles = () => {
         .then(data => setBottles(data));
     },[])
 
+    const handleAddToCart = bottle => {
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+    }
+
     return (
         <div>
-            <h2>Bottles Here: {bottles.length}</h2>
+            <h2>Bottles Available: {bottles.length}</h2>
+            <h3>Cart: {cart.length}</h3>
             <div className="bottle-container">
             {
                 bottles.map(bottle => <Bottle key={bottle.id}
                 bottle={bottle}
+                handleAddToCart={handleAddToCart}
                 ></Bottle>)
             }
             </div>
